3b9801fa2e1c3d5fd6115693c9e1dd5183ef008e
Dmytro Borysenko <borisand020774@gmail.com>
2025-06-25 13:22:36 +0300

    https://github.com/DmytroBorysenko2004/jv-dependency-injection

--- src/main/java/mate.academy/Main.java
+++ src/main/java/mate.academy/Main.java
@@ -9,8 +9,8 @@ public class Main {
 
     public static void main(String[] args) {
         // Please test your Injector here. Feel free to push this class as a part of your solution
-        Injector injector = Injector.getInjector();
-        ProductService productService = null;
+        ProductService productService = (ProductService) Injector.getInjector()
+                .getInstance(ProductService.class);
         List<Product> products = productService.getAllFromFile("products.txt");
         products.forEach(System.out::println);
     }
--- src/main/java/mate.academy/lib/Injector.java
+++ src/main/java/mate.academy/lib/Injector.java
@@ -1,13 +1,60 @@
 package mate.academy.lib;
 
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.Map;
+import mate.academy.service.FileReaderService;
+import mate.academy.service.ProductParser;
+import mate.academy.service.ProductService;
+import mate.academy.service.impl.FileReaderServiceImpl;
+import mate.academy.service.impl.ProductParserImpl;
+import mate.academy.service.impl.ProductServiceImpl;
+
 public class Injector {
     private static final Injector injector = new Injector();
+    private final Map<Class<?>, Object> instanceCache = new HashMap<>();
+
+    private final Map<Class<?>, Class<?>> interfaceToImpl = Map.of(
+            FileReaderService.class, FileReaderServiceImpl.class,
+            ProductParser.class, ProductParserImpl.class,
+            ProductService.class, ProductServiceImpl.class
+    );
 
     public static Injector getInjector() {
         return injector;
     }
 
     public Object getInstance(Class<?> interfaceClazz) {
-        return null;
+        Class<?> implClass = interfaceToImpl.get(interfaceClazz);
+        if (implClass == null) {
+            throw new RuntimeException("No implementation mapped for " + interfaceClazz.getName());
+        }
+
+        if (!implClass.isAnnotationPresent(Component.class)) {
+            throw new RuntimeException("Class " + implClass.getName()
+                    + " is not annotated with @Component");
+        }
+
+        if (instanceCache.containsKey(implClass)) {
+            return instanceCache.get(implClass);
+        }
+
+        try {
+            Object instance = implClass.getDeclaredConstructor().newInstance();
+
+            for (Field field : implClass.getDeclaredFields()) {
+                if (field.isAnnotationPresent(Inject.class)) {
+                    Class<?> fieldType = field.getType();
+                    Object dependency = getInstance(fieldType);
+                    field.setAccessible(true);
+                    field.set(instance, dependency);
+                }
+            }
+
+            instanceCache.put(implClass, instance);
+            return instance;
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to create instance of: " + implClass.getName(), e);
+        }
     }
 }
